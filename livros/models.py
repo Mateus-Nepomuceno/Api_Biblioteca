3ae1be4f2eafa43b2edc837cefd53e71ae75b67c
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-02-28 22:09:38 -0300

    fix: corrigindo carregamento do .env e atributos do modelo livro

--- livros/models.py
+++ livros/models.py
@@ -14,7 +14,7 @@ class Livro (models.Model):
     autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
     titulo = models.CharField(max_length=200)
     resumo = models.TextField()
-    data_pub = models.DateField()
+    ano_pub = models.IntegerField()
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
